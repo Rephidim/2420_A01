d6736cd61fb7fb2392216e8d2eaae828f86acb4e
splattershot <35549149+splattershot@users.noreply.github.com>
2018-01-27 12:55:53 -0700

    Add files via upload

--- Percolation.java
+++ Percolation.java
@@ -1,107 +1,135 @@
 import java.util.Arrays;
+import java.util.Random;
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-/*
- * TODO open() works to open individual sites in 
- * grid.
- * Side-to-side filling works properly.
- * Top-Bottom filling not yet implemented.
- */
-
 public class Percolation {
-	private int[] a;
-	private static WeightedQuickUnionUF uf;
-	private int w;
 	
-	/**
-	 * Constructor - creates a new boolean array of length N + 2, sets all values to false, then sets
-	 * values at b[0] and b[N-1] to true.
-	 * @param N
-	 */
-	public Percolation(int N) {
-		this.a = new int[N*N + 2];
-		this.w = N;
-		Arrays.fill(a, 0);
-		a[0] = 1;
-		a[a.length -1] = 1;
-		uf = new WeightedQuickUnionUF(a.length);
+	private static boolean[] grid;
+	private static WeightedQuickUnionUF percTracker, isFullTracker;
+	static int gridWidth;
+	
+	
+	public Percolation(int n) {
+		//track grid width
+		gridWidth = n;
+		
+		//create a grid of n by n size and a virtual top and bottom site
+		grid = new boolean[(n * n) + 2];
+	
+		//create union object to track isfull and percolates
+		percTracker = new WeightedQuickUnionUF((n * n) + 2);
+		isFullTracker = new WeightedQuickUnionUF((n * n) + 1);
+		
+		//open the top site
+		grid[0] = true;
+	
 	}
 	
-	public void open(int i, int j) {
-		int p = xyConv(i,j);
-		if (a[p] == 0) {
-			a[p] = 1;
+	public void open (int i, int j) {
+		grid[convertXY(i, j)] = true;
+		
+		if(j == 0) {
+			percTracker.union(0,convertXY(i,j));
+			isFullTracker.union(0,convertXY(i,j));
 		}
-		if (j == 0 && a[p] == 1) {
-			uf.union(0, p);
+		
+		if(isOpen(i - 1, j)) {
+			percTracker.union(convertXY(i -1, j), convertXY(i, j));
+			if(isFull(i - 1, j)) {
+				isFullTracker.union(convertXY(i -1, j), convertXY(i, j));
+			}
 		}
-		connectH(i, j);
-		connectV(i, j);
+		if(isOpen(i + 1, j)) {
+			percTracker.union(convertXY(i + 1, j), convertXY(i, j));
+			if(isFull(i + 1, j)) {
+				isFullTracker.union(convertXY(i + 1, j), convertXY(i, j));
+			}
+		}
 		try {
-			if (j == this.w-1 && a[p] == 1) {
-				if (isFull(i-1,j) || isFull(i+1,j) || isFull(i,j-1)) {
-					uf.union(p,a.length-1);
+			if(isOpen(i, j - 1)) {
+				percTracker.union(convertXY(i, j - 1), convertXY(i, j));
+				if(isFull(i, j - 1)) {
+					isFullTracker.union(convertXY(i, j - 1), convertXY(i, j));
 				}
 			}
-		} catch (IndexOutOfBoundsException e) {}
-	}
-
-	/**
-	 * @param i
-	 * @param j
-	 */
-	private void connectV(int i, int j) {
-		int p = xyConv(i,j);
-		try {
-			if (a[p] == 1 && a[xyConv(i,j+1)] == 1) {
-				uf.union(p,xyConv(i,j+1));
+			
+			if(isOpen(i, j + 1)) {
+				percTracker.union(convertXY(i, j + 1), convertXY(i, j));
+				if(isFull(i, j + 1)) {
+					isFullTracker.union(convertXY(i, j + 1), convertXY(i, j));
+				}
 			}
-		} catch (IndexOutOfBoundsException e) {}
-		try {
-			if (a[p] == 1 && a[xyConv(i,j-1)] == 1) {
-				uf.union(p,xyConv(i,j-1));
-			}
-		} catch (IndexOutOfBoundsException e) {}
-	}
-
-	/**
-	 * @param i
-	 * @param j
-	 */
-	private void connectH(int i, int j) {
-		int p = a[xyConv(i,j)];
-		if (p == 1 && a[xyConv(i-1,j)] == 1) {
-			uf.union(xyConv(i,j),xyConv(i-1,j));
-		}	
-		if (p == 1 && a[xyConv(i+1,j)] == 1) {
-			uf.union(xyConv(i,j),xyConv(i+1,j));
+			
+		}catch(ArrayIndexOutOfBoundsException e) {
+			
 		}
+		
 	}
 	
-	/**
-	 * Check if value at b[i] is true
-	 * @param i
-	 * @return x
-	 */
 	public boolean isOpen(int i, int j) {
-		if (a[xyConv(i,j)] == 1) {
-			return true;
-		} return false;
+		return grid[convertXY(i, j)];
 	}
 	
 	public boolean isFull(int i, int j) {
-		//TODO is site (i,j) full?
-		if (uf.connected(0,xyConv(i,j))) {
-			return true;
-		}
-		return false;
+		return isFullTracker.connected(0, convertXY(i, j));
 	}
 	
 	public boolean percolates() {
-		return uf.connected(0, a.length - 1);
+		return percTracker.connected(0, (grid.length - 1));
 	}
 	
-	private int xyConv(int i, int j) {
-		return (i+1) + (this.w * j);
+	
+	public int convertXY(int x, int y) {
+		return (x+1)+  (y * this.gridWidth);
+				
 	}
+	
+	public int[] convertIndex(int index) {
+		int[] orderedPair = {index % gridWidth, index / gridWidth};
+		return orderedPair;
+	}
+	
+	
+	public void printGrid() {
+		for(int i =0; i < grid.length; i++) {
+			if(grid[i]) {
+				if(isFull(convertIndex(i)[0], convertIndex(i)[1]));
+			}
+			if(i%gridWidth == 0) {
+				System.out.println();
+			}
+		}
+	}
+	
+	public static void main(String [] args) {
+		
+		Percolation p = new Percolation(10);
+		
+		Random rand = new Random();	
+		
+		for (int i = 0; i < 51; i++) {
+			
+			int randomX = rand.nextInt(gridWidth);
+			int randomY = rand.nextInt(gridWidth);
+			
+			//if the chosen index is already open choose a different one.
+			if(grid[p.convertXY(randomX, randomY)]) {
+				i--;
+			}else {
+				//open the index
+				p.open(randomX, randomY);
+					}
+					
+				}
+		
+		
+		p.printGrid();
+		
+	}
+
 }
+
+
+
+
